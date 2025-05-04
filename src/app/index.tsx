d1b88a7a7661fc29cc38bb844bbef84f94b381c1
unknown <viniciussouzaramospv@gmail.com>
2025-05-04 00:42:06 -0300

    feat: Estruturação de pastas e configuração de arquivos

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View style={{ flex: 1, padding: 40, gap: 40 }}>
+         <Text>Hello-Word</Text>
+        </View>
+      )
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+    yellow: {
+        base: "#FFCF78",
+        dark: "#F2B84D"
+    },
+    white: {
+        base: "#FFF"
+    },
+    black:{
+        base: "#000"
+    },
+    gray: {
+        base: "#D9D9D9"
+    }
+}
